57caa65fdd0efdc3af06df456685b306f665f741
Bill Mill <bill.mill@gmail.com>
2010-03-21 12:26:34 -0400

    big refactoring works for analyze.py, has probably broken bracket.py

--- analyze.py
+++ analyze.py
@@ -19,7 +19,7 @@ class KenPomStrictModel(object):
     def __call__(self, game):
         t1, t2 = game.team1, game.team2
         #compare pomeroy ratings and pick the higher one
-        a, b, = t1[1], t2[1]
+        a, b = t1.pyth, t2.pyth
         if a > b:
             return t1
         return t2
@@ -34,12 +34,12 @@ class KenPomRandomModel(object):
     def __call__(self, game):
         t1, t2 = game.team1, game.team2
         #compare pomeroy ratings and pick the higher one
-        fav, dog = maxmin(t1[1], t2[1])
+        fav, dog = maxmin(t1.pyth, t2.pyth)
         pct = log5(fav, dog)
         for i in range(self.reps):
             if rand() < pct:
-                return t1 if fav == t1[1] else t2
-        return t1 if dog == t1[1] else t2
+                return t1 if fav == t1.pyth else t2
+        return t1 if dog == t1.pyth else t2
 
     def __str__(self): return "Pomeroy Random Model %s reps" % self.reps
 
@@ -49,9 +49,9 @@ class SeedModel(object):
 
     def __call__(self, game):
         t1, t2 = game.team1, game.team2
-        if game.seed1 == game.seed2:
+        if t1.seed == t2.seed:
             return t1 if rand() < .5 else t2
-        return t1 if game.seed1 < game.seed2 else t2
+        return t1 if t1.seed < t2.seed else t2
 
     def __str__(self): return "Seed Model"
 
@@ -60,22 +60,23 @@ class PureRandomModel(object):
         self.mutable = True
 
     def __call__(self, game):
-        t1, t2 = game.team1, game.team2
-        return t1 if rand() < .5 else t2
+        return game.team1 if rand() < .5 else game.team2
 
     def __str__(self): return "Pure Random Model"
 
 models = [KenPomStrictModel(), KenPomRandomModel(1), KenPomRandomModel(2),
           KenPomRandomModel(3), SeedModel(), PureRandomModel()]
+#DEBUGGING TODO DELETEME
+#models = [KenPomStrictModel()]
 kenpom = get_kenpom()
 games = get_games()
 
 for round in actual_winners:
     for team in actual_winners[round]:
-        assert(team in kenpom)
+        assert any(True for t in kenpom if t.name==team)
         for game in (g for g in games if g.round==round):
-            if game.team1[0] == team or game.team2[0] == team:
-                game.winner = team
+            if game.team1.name == team:   game.winner = game.team1; break
+            elif game.team2.name == team: game.winner = game.team2; break
 
 def average(a):
     """Average a list of tuples of the form (value, number of items with that value)"""
@@ -87,8 +88,23 @@ def average(a):
     return sum/float(total)
 
 def possible_points(games):
-    """how many points could still be scored?"""
-    pass #TODO
+    """points scored + how many points could still be scored?"""
+    p = 0
+    eliminated = set([])
+    for round in range(1,7):
+        this_rounds_pts = 2**(round-1)
+        for game in (g for g in games if g.round==round):
+            if hasattr(game, 'winner'):
+                if game.winner == game.predicted:
+                    #if the game was correctly predicted, add the points
+                    p += this_rounds_pts
+                else:
+                    #otherwise, the predicted team has been eliminated
+                    eliminated.add(game.predicted)
+            else:
+                if game.predicted not in eliminated:
+                    p += this_rounds_pts
+    return p
 
 for model in models:
     repetitions = 1000 if model.mutable else 1
@@ -100,7 +116,8 @@ for model in models:
         for round in range(1,7):
             for game in (g for g in games if g.round==round):
                 game.predicted = model(game)
-                if game.winner and game.winner == game.predicted[0]:
+                game.advance(game.predicted)
+                if hasattr(game, 'winner') and game.winner == game.predicted:
                     ncorrect += 1
 
         if ncorrect not in ncorrect_bins:
@@ -115,6 +132,8 @@ for model in models:
             possible_point_bins[p] += 1
 
 
-    print "model %s avg: %s, distribution: %s" % (str(model),
+    print "model %s avg: %s, distribution: %s, possible points: %s" % (
+        str(model),
         average(list(ncorrect_bins.iteritems())),
-        sorted([(k,v) for k,v in ncorrect_bins.iteritems()]))
+        sorted([(k,v) for k,v in ncorrect_bins.iteritems()]),
+        sorted([(k,v) for k,v in possible_point_bins.iteritems()]))
--- get_games.py
+++ get_games.py
@@ -1,6 +1,45 @@
 import re
 from math import ceil, floor
 
+class Team(object):
+    def __init__(self, name, pyth, adjo, adjd, seed=0):
+        self.name = name
+        self.pyth = pyth
+        self.adjo = adjo
+        self.adjd = adjd
+        self.seed = seed
+
+    def __repr__(self):
+        if self.seed:
+            return "%s) %s (%s, %s, %s)" % (
+                self.seed, self.name, self.pyth, self.adjo, self.adjd)
+        else:
+            return "%s (%s, %s, %s)" % (
+                self.name, self.pyth, self.adjo, self.adjd)
+
+class Game(object):
+    def __init__(self, region, round, gameno, team1=None, team2=None):
+        self.region = region
+        self.round = round
+        self.gameno = gameno
+        self.team1 = team1
+        self.team2 = team2
+        self.child = None
+        #is this the upper game or the lower game? <bool>
+        self.isupper = None
+        self.rows = [None, None]
+
+    def advance(self, team):
+        assert (self.team1 == team) or (self.team2 == team)
+        if self.isupper:
+            self.child.team1 = team
+        else:
+            self.child.team2 = team
+
+    def __repr__(self):
+        return "%s vs %s round %s game %s region %s rows %s" % (self.team1[0],
+            self.team2[0], self.round, self.gameno, self.region, self.rows)
+
 def get_kenpom():
     """return a list of tuples:
     (rank, name, record, pythagorean score, adjusted O, adj O rank, adjusted D, adj D rank)"""
@@ -16,7 +55,7 @@ def get_kenpom():
             team[j] = float(team[j])
         kenpom[i] = tuple(team)
 
-    return dict((x[1], x) for x in kenpom)
+    return [Team(team[1], team[3], team[4], team[6]) for team in kenpom]
 
 def get_games():
     bracket = eval(file("2010teams.dat").read())
@@ -28,9 +67,9 @@ def get_games():
         for t in bracket[r].values():
             if type(t) == type([]):
                 for t2 in t:
-                    assert(t2 in kenpom)
+                    assert any(True for team in kenpom if t2 == team.name)
             else:
-                assert(t in kenpom)
+                assert any(True for team in kenpom if t == team.name)
 
     #let's build a dictionary {"region" -> {seed -> [name, pyth, adjo, adjd]}}
     merged = {}
@@ -39,29 +78,16 @@ def get_games():
         for seed, teamname in bracket[region].items():
             if type(teamname) == type([]):
                 t1, t2 = teamname 
-                rank, name, record, pyth, adjo, adjorank, adjd, adjdrank = kenpom[t1]
-                merged[region][16] = [name, pyth, adjo, adjd]
-                rank, name, record, pyth, adjo, adjorank, adjd, adjdrank = kenpom[t2]
-                merged[region][17] = [name, pyth, adjo, adjd]
+                team = [t for t in kenpom if t.name == t1][0]
+                team.seed = 16
+                merged[region][16] = team
+                team = [t for t in kenpom if t.name == t2][0]
+                team.seed = 17
+                merged[region][17] = team
             else:
-                rank, name, record, pyth, adjo, adjorank, adjd, adjdrank = kenpom[teamname]
-                merged[region][seed] = [name, pyth, adjo, adjd]
-
-    class Game(object):
-        def __init__(self, region, round, gameno, seed1=None, team1=None, seed2=None, team2=None):
-            self.region = region
-            self.round = round
-            self.gameno = gameno
-            self.seed1 = seed1 or ""
-            self.team1 = team1 or [""]
-            self.seed2 = seed2 or ""
-            self.team2 = team2 or [""]
-            self.child = None
-            self.rows = [None, None]
-
-        def __repr__(self):
-            return "%s vs %s round %s game %s region %s rows %s" % (self.team1[0],
-                self.team2[0], self.round, self.gameno, self.region, self.rows)
+                team = [t for t in kenpom if t.name == teamname][0]
+                team.seed = seed
+                merged[region][seed] = team
 
     games = []
     game = 1
@@ -70,9 +96,21 @@ def get_games():
             oppseed = 17-seed
             t1 = merged[region][seed]
             t2 = merged[region][oppseed]
-            games.append(Game(region, 1, game, seed, t1, oppseed, t2))
+            games.append(Game(region, 1, game, t1, t2))
             game += 1
-    return games
+
+    for round in (2,3,4):
+      i = iter(g for g in games if g.round==round-1)
+      roundg = []
+      for g in i:
+        g2 = i.next()
+        gg = Game(g.region, round, game)
+        roundg.append(gg)
+        g.child = g2.child = gg
+        g.isupper = True
+        g2.isupper = False
+        game += 1
+      games.extend(roundg)
 
     rf1, rf2 = [g for g in games if g.round==4 and g.region in ("Midwest", "West")]
     rf3, rf4 = [g for g in games if g.round==4 and g.region in ("East", "South")]
@@ -81,12 +119,21 @@ def get_games():
     games.append(ff1)
     games.append(ff2)
     rf1.child = rf2.child = ff1
+    rf1.isupper = True
+    rf2.isupper = False
     rf3.child = rf4.child = ff2
+    rf3.isupper = True
+    rf4.isupper = False
 
     c = Game("final four", 6, game+2)
     games.append(c)
     ff1.child = ff2.child = c
+    ff1.isupper = True
+    ff2.isupper = False
 
     winner = Game("final four", 7, game+3)
     games.append(winner)
     c.child = winner
+    c.isupper = True
+
+    return games
