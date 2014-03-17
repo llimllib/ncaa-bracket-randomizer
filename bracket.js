22fb3cfc5ec1480624a76e3a352449b39c07725e
Bill Mill <bill.mill@gmail.com>
2014-03-17 19:29:56 -0400

    replacing index with test

--- bracket.js
+++ bracket.js
@@ -12,9 +12,15 @@ function nextgame(round, gid) {
 
 // return a if a wins, else b
 function whowins(a, b, randomness) {
-  var fav = Math.max(a, b);
-  var dog = Math.min(a, b);
-  var odds = log5(fav, dog);
+  if (a.rating > b.rating) {
+    var fav = a;
+    var dog = b;
+  } else {
+    var fav = b;
+    var dog = a;
+  }
+
+  var odds = log5(fav.rating, dog.rating);
 
   // TODO return of the randomness slider
   if (randomness == "0") {
@@ -56,28 +62,72 @@ function drawWinners() {
     d3.selectAll(".round" + round + " text").remove();
 
     d3.selectAll(".round" + round)
+      .filter(function(d) { return d.round < 5; })
       .append("text")
-        .attr("x", function(d) { return d.round * roundwidth + 3; })
-        .attr("y", function(d) { return bottomliney(d) - 1; })
-        .style("font", "10px sans-serif")
-        .text(function(d) { if (d.bottomteam) { return d.bottomteam.name; }});
-
-    d3.selectAll(".round" + round)
-      .append("text")
-        .attr("x", function(d) { return d.round * roundwidth + 3; })
-        .attr("y", function(d) { return topliney(d) - 1; })
+        .attr("x", 4)
+        .attr("y", lineheight)
         .style("font", "10px sans-serif")
         .text(function(d) { if (d.topteam) { return d.topteam.name; }});
+
+    d3.selectAll(".round" + round)
+      .filter(function(d) { return d.round < 5; })
+      .append("text")
+        .attr("x", 4)
+        .attr("y", function(d) { return innerpadding(d) + 2*lineheight + textpadding; })
+        .style("font", "10px sans-serif")
+        .text(function(d) { if (d.bottomteam) { return d.bottomteam.name; }});
   }
 
-  var lastline = (lineheight * ((Math.pow(2, 6)-1) / 2) + lineheight);
+  d3.select("#game61")
+    .append("text")
+    .attr("x", 2)
+    .attr("y", lineheight)
+    .style("font", "10px sans-serif")
+    .text(function(d) { if (d.topteam) { return d.topteam.name; }});
 
-  d3.select(".winner text").remove();
+  d3.select("#game61")
+    .append("text")
+    .attr("x", 2)
+    .attr("y", function(d) { return innerpadding(d) + 2*lineheight + textpadding; })
+    .style("font", "10px sans-serif")
+    .text(function(d) { if (d.topteam) { return d.bottomteam.name; }});
+
+  d3.select("#game62")
+    .append("text")
+    .attr("x", 2)
+    .attr("y", lineheight)
+    .style("font", "10px sans-serif")
+    .text(function(d) { if (d.topteam) { return d.topteam.name; }});
+
+  d3.select("#game62")
+    .append("text")
+    .attr("x", 2)
+    .attr("y", function(d) { return innerpadding(d) + 2*lineheight + textpadding; })
+    .style("font", "10px sans-serif")
+    .text(function(d) { if (d.topteam) { return d.bottomteam.name; }});
+
+  d3.select("#game63")
+    .append("text")
+    .attr("x", 2)
+    .attr("y", lineheight-2)
+    .style("font", "10px sans-serif")
+    .text(function(d) { if (d.topteam) { return d.topteam.name; }});
+
+  d3.select("#game63")
+    .append("text")
+    .attr("x", 30)
+    .attr("y", function(d) { return innerpadding(d) + 2*lineheight + textpadding; })
+    .style("font", "10px sans-serif")
+    .text(function(d) { if (d.topteam) { return d.bottomteam.name; }});
+
+  d3.selectAll(".winner text").remove();
   d3.select(".winner")
     .append("text")
-    .attr("x", 7 * roundwidth + 3)
-    .attr("y", lastline - 1)
-    .style("font", "10px sans-serif")
+    .attr("x", 50)
+    .attr("y", 20)
+    .attr("text-anchor", "middle")
+    .style("font", "14px sans-serif")
+    .style("font-weight", "bold")
     .text(function(d) { return d.winner; });
 }
 
@@ -88,8 +138,8 @@ function randomize() {
       var nextgid = nextgame(d.round, d.gid);
       if (!(nextgid >= 1 && nextgid <= 64)) { throw new Error("invalid gid ", gid, d.round, d.gid); }
 
-      var winner = whowins(d.topteam.rating, d.bottomteam.rating, randomness);
-      if (winner == d.topteam.rating) {
+      var winner = whowins(d.topteam, d.bottomteam, randomness);
+      if (winner == d.topteam) {
         //console.log(d.topteam.name, d.topteam.rating, "beats", d.bottomteam.name, d.bottomteam.rating)
         advance(d.topteam, nextgid, d.gid);
       } else {
@@ -118,43 +168,25 @@ function set(region, gameoffset, topteam, bottomteam) {
   game.bottomteam = bottomteam;
 }
 
-roundwidth = 100;
-lineheight = 14;
-gameheight = 30;
-linecolor = "#ccc";
+var width = 1020,
+    height = 700,
+    roundwidth = 90,
+    lineheight = 18,
+    textpadding = 2, // space between bracket line and text
+    gameheight = 30,
+    linecolor = "#ddd";
 
-function topliney(game) {
-  // spacing at the top goes half of 0, 1, 3, 7, 15...
-  var topspace = lineheight * ((Math.pow(2, game.round-1)-1) / 2);
-  // the first line is always at ts + lh
-  var start = topspace + lineheight;
-
-  // 1, 2, 4, 8, ...
-  var roundheight = lineheight * Math.pow(2, game.round-1);
-  return start + (game.roundgame-1)*(2*roundheight);
-}
-
-function bottomliney(game) {
-  // spacing at the top goes half of 0, 1, 3, 7, 15...
-  var topspace = lineheight * ((Math.pow(2, game.round-1)-1) / 2);
-  // the first line is always at ts + lh
-  var start = topspace + lineheight;
-
-  // 1, 2, 4, 8, ...
-  var roundheight = lineheight * Math.pow(2, game.round-1);
-  return start + (game.roundgame-1)*(2*roundheight) + roundheight;
+var ipad = {1: 0, 2: 22, 3: 63, 4: 145, 5: 335, 6: 258};
+function innerpadding(d) {
+  return ipad[d.round];
 }
 
 function main() {
-  var width = 800,
-      height = 900,
-      mx = 30, //x margin
-      my = 25; //y margin
 
   var bracket = d3.select("#bracket").append("svg")
       .attr("class", "bracket")
-      .attr("width", width + 2*mx)
-      .attr("height", height + 2*my);
+      .attr("width", width)
+      .attr("height", height);
 
   var line = d3.svg.line();
 
@@ -162,14 +194,14 @@ function main() {
     if ((gid >= 1 && gid <= 8) || (gid >= 33 && gid <= 36) ||
         (gid >= 49 && gid <= 50) || (gid == 57)) { return "south"; }
     if ((gid >= 9 && gid <= 16) || (gid >= 37 && gid <= 40) ||
-        (gid >= 51 && gid <= 52) || (gid == 58)) { return "west"; }
+        (gid >= 51 && gid <= 52) || (gid == 58)) { return "east"; }
     if ((gid >= 17 && gid <= 24) || (gid >= 41 && gid <= 44) ||
-        (gid >= 53 && gid <= 54) || (gid == 59)) { return "east"; }
+        (gid >= 53 && gid <= 54) || (gid == 59)) { return "west"; }
     if ((gid >= 25 && gid <= 32) || (gid >= 45 && gid <= 48) ||
         (gid >= 55 && gid <= 56) || (gid == 60)) { return "midwest"; }
-    if (gid == 61) { return "south-west"; }
-    if (gid == 62) { return "east-midwest"; }
-    if (gid == 63) { return "south-west-east-midwest"; }
+    if (gid == 61) { return "south-east"; }
+    if (gid == 62) { return "west-midwest"; }
+    if (gid == 63) { return "south-east-west-midwest"; }
 
     // raise an error if we fall through
     throw new Error("undefined region for gid " + gid);
@@ -190,18 +222,21 @@ function main() {
   // create the games array without teams
   $.each(rounds, function(i, round) {
     var roundgame = 1;
+    var regiongame = {"south": 1, "west": 1, "east": 1, "midwest": 1, "south-east": 1, "west-midwest": 1};
     for (var i=0; i < ngames; i++) {
       gid += 1;
-      // TODO set the region
+      region = gameregion(gid);
       games.push({
         gid: gid,
         round: round,
         roundgame: roundgame,
-        region: gameregion(gid),
+        region: region,
+        regiongame: regiongame[region],
         topteam: undefined,
         bottomteam: undefined
       });
       roundgame += 1;
+      regiongame[region] += 1;
     }
     ngames /= 2;
   });
@@ -218,7 +253,7 @@ function main() {
       set(region, 8, seeds["2"], seeds["15"]);
     });
 
-    gamegs = bracket.selectAll(".game")
+    gamedivs = bracket.selectAll(".game")
       .data(games)
       .enter()
       .append("g")
@@ -227,59 +262,101 @@ function main() {
 
     linewidth = 1;
 
-    // Top line
-    gamegs.append("rect")
-      .attr("x", function(d) { return d.round * roundwidth; })
-      .attr("y", topliney)
-      .attr("width", roundwidth)
-      .attr("height", linewidth)
-      .attr("fill", linecolor);
+    gamegs = d3.selectAll(".game").filter(function(g) { return g.gid < 63; });
+
+    // top line
+    gamegs.append("line")
+      .attr("x1", 0)
+      .attr("x2", roundwidth)
+      .attr("y1", lineheight + textpadding)
+      .attr("y2", lineheight + textpadding)
+      .style("stroke", linecolor);
 
     // bottom line
-    gamegs.append("rect")
-      .attr("x", function(d) { return d.round * roundwidth; })
-      .attr("y", bottomliney)
-      .attr("width", roundwidth)
-      .attr("height", linewidth)
-      .attr("fill", linecolor);
+    gamegs.append("line")
+      .attr("x1", 0)
+      .attr("x2", roundwidth)
+      .attr("y1", function(d) { return innerpadding(d) + 2*lineheight + 2*textpadding; })
+      .attr("y2", function(d) { return innerpadding(d) + 2*lineheight + 2*textpadding; })
+      .style("stroke", linecolor);
+
+    function rightline(d) {
+      if (d.region == "west" || d.region == "midwest" || d.region == "west-midwest") {
+        return 0;
+      } else {
+        return roundwidth;
+      }
+    }
 
     // right line
-    gamegs.append("rect")
-      .attr("x", function(d) { return d.round * roundwidth + roundwidth; })
-      .attr("y", topliney)
-      .attr("width", linewidth)
-      .attr("height", function(d) { return bottomliney(d) - topliney(d) + linewidth; })
-      .attr("fill", linecolor);
+    gamegs.append("line")
+      .attr("x1", rightline)
+      .attr("x2", rightline)
+      .attr("y1", lineheight + textpadding)
+      .attr("y2", function(d) { return innerpadding(d) + 2*lineheight + 2*textpadding; })
+      .style("stroke", linecolor);
 
     //top teams
     gamegs.append("text")
-      .attr("x", function(d) { return d.round * roundwidth; })
-      .attr("y", function(d) { return topliney(d) - 1; })
+      .attr("x", 0)
+      .attr("y", lineheight)
       .style("font", "10px sans-serif")
       .text(function(d) { if (d.topteam) {return d.topteam.seed + ". " + d.topteam.name; }});
 
     //bottom teams
     gamegs.append("text")
-      .attr("x", function(d) { return d.round * roundwidth; })
-      .attr("y", function(d) { return bottomliney(d) - 1; })
+      .attr("x", 0)
+      .attr("y", function(d) { return innerpadding(d) + 2* lineheight + textpadding; })
       .style("font", "10px sans-serif")
-      .text(function(d) { if (d.bottomteam) { return d.bottomteam.seed + ". " + d.bottomteam.name; }});
+      .text(function(d) { if (d.bottomteam) {return d.bottomteam.seed + ". " + d.bottomteam.name; }});
 
-    var lastline = (lineheight * ((Math.pow(2, 6)-1) / 2) + lineheight);
+    var regionheight = {"south": 0, "west": 0, "east": height/2, "midwest": height/2,
+                        "south-east": 0, "west-midwest": 0};
+    var roundheight = {1: 0, 2: 10, 3: 30, 4: 75, 5: 155, 6: 80};
+    var gamepadding = {1: 5, 2: 24, 3: 66, 4: 0, 5: 0, 6: 0};
 
-    var winner = [{"winner": undefined}];
+    function layout(d) {
+      var gameheight = 2*lineheight + innerpadding(d);
+      var y = regionheight[d.region] + roundheight[d.round] + (d.regiongame-1) * (gameheight + gamepadding[d.round]);
+      // these are on the right side
+      if (d.region == "west" || d.region == "midwest" || d.region == "west-midwest") {
+        var x = width-(d.round)*roundwidth;
+      } else {
+        var x = (d.round-1)*roundwidth;
+      }
+      return "translate("+x+","+y+")"
+    }
 
-    bracket.selectAll(".winner")
-      .data(winner)
-      .enter()
-      .append("g")
-      .attr("class", "winner round7")
-      .append("rect")
-      .attr("x", 7 * roundwidth)
-      .attr("y", lastline)
-      .attr("width", roundwidth)
-      .attr("height", linewidth)
-      .attr("fill", linecolor);
+    gamegs.attr("transform", layout);
+
+    // final
+    var champ = d3.select("#game63")
+      .attr("transform", "translate(450, 205)");
+    champ.append("line")
+      .attr("x1", 0)
+      .attr("x2", roundwidth)
+      .attr("y1", lineheight)
+      .attr("y2", lineheight)
+      .style("stroke", linecolor);
+    champ.append("line")
+      .attr("x1", 30)
+      .attr("x2", roundwidth+30)
+      .attr("y1", 300)
+      .attr("y2", 300)
+      .style("stroke", linecolor);
+
+    // winner
+    winnerbox = bracket.append("g")
+      .data([{winner: undefined}])
+      .attr("transform", "translate(460, 335)")
+      .attr("class", "winner");
+    winnerbox.append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", 100)
+      .attr("height", 30)
+      .style("fill", "white")
+      .style("stroke", "#bbb")
   });
 }
 
